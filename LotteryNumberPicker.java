409e170dd35734db785ee62f77b89b404d6f59ab
SouthxPaw <40015860+SouthxPaw@users.noreply.github.com>
2018-07-02 18:18:57 -0400

    LotteryNumber java file
    This is the bare bone skeleton of our code. Please add what you can and want to then let me know so I can test it and see if it needs to be adjusted or changed in any way.
    
    Thanks!

---
+++ LotteryNumberPicker.java
@@ -0,0 +1,42 @@
+package LNum;
+
+import java.util.*;
+import java.awt.*;
+
+//this is just a skeleton of our code.
+//feel free to reupload to github, so I(Jordan) can test it
+//and see if it needs to be fixed or adjusted
+
+public class LotteryNumberPicker {
+
+	//initilizations
+	int lottoNum, winNum, numOfTickets, hotNums;
+	
+	//constructor for class
+	public LotteryNumberPicker(int lottoNum, int winNum, int numOfTickets, int hotNums) {
+		this.lottoNum = lottoNum;
+		this.winNum = winNum;
+		this.numOfTickets = numOfTickets;
+		this.hotNums = hotNums;
+	}
+	
+	//undeclared
+	public int getLottoNum() {
+		return lottoNum;
+	}
+	
+	//Sarah
+	public int getWinningNum() {
+		
+	}
+	
+	//Anthony
+	public int getNumOfTickets(){
+		
+	}
+	
+	//undeclared
+	public int getHotNums() {
+		
+	}
+}
