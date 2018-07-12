9bd1637ac8ea854c2a7683db39f8d3a9bdf66041
SouthxPaw <40015860+SouthxPaw@users.noreply.github.com>
2018-07-11 21:50:02 -0400

    Add files via upload
    so this is the updated code. I left notes in the code where I'm sure it's having trouble.

--- GameChoice.java
+++ GameChoice.java
@@ -6,6 +6,7 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GameChoice extends JFrame implements ActionListener  {
+
 	
 	//intializations
 	public static JPanel gameChoicePanel;
@@ -17,66 +18,88 @@ public class GameChoice extends JFrame implements ActionListener  {
 	public static JButton Fantasy5Button;
 	public static JButton PowerballButton;
 	public static JButton MegaMillionsButton;
-    public static ImageIcon fantasy5Icon = new ImageIcon("Fantasy5Logo.jpg");
-    public static ImageIcon megaMillionsIcon = new ImageIcon("MegaMillionsLogo.png");
-    public static ImageIcon powerballIcon = new ImageIcon("PowerballLogo.png");
+	public static ImageIcon fantasy5Icon = new ImageIcon("Fantasy5Logo.jpg");
+	public static ImageIcon megaMillionsIcon = new ImageIcon("MegaMillionsLogo.png");
+	public static ImageIcon powerballIcon = new ImageIcon("PowerballLogo.png");
 	public static JFrame frame;
-    
-    
-    
+	private int numClicks = 0;
+
+
 	private static final long serialVersionUID = 1L;
 	//This initializes the labels and buttons.
 	JLabel l1, l2;
 	JButton b1,b2,b3, b4;
 	JTextField t1, t2;
-	GameChoice()
+	public GameChoice()
 	{
-	
-    //This is to add text to the labels & buttons.
-	l1 = new JLabel("How Many Tickets? ");
-	l2 = new JLabel("Number of Quick Picks");
-	b1 = new JButton("SUBMIT");
-	b2 = new JButton("EXIT");
-	b3 = new JButton("I WANT RANDOM NUMBERS");
-	b4 = new JButton("HOTTEST NUMBERS");
-	t1 = new JTextField(30);
-	t2 = new JTextField(30);
 
-	//This is to add the labels and buttons.
-	add(l1);
-	add(t1);
-	add(l2);
-	add(t2);
-	add(b1);
-	add(b2);
-	add(b3);
-	add(b4);
+		//This is to add text to the labels & buttons.
+		l1 = new JLabel("How Many Tickets? ");
+		l2 = new JLabel("Number of Quick Picks");
+		b1 = new JButton("SUBMIT");
+		b2 = new JButton("EXIT");
+		b3 = new JButton("I WANT RANDOM NUMBERS");
+		b4 = new JButton("HOTTEST NUMBERS");
+		t1 = new JTextField(30);
+		t2 = new JTextField(30);
 
-	b2.addActionListener(new CloseListener());
+		//This is to add the labels and buttons.
+		add(l1);
+		add(t1);
+		add(l2);
+		add(t2);
+		add(b1);
+		add(b2);
+		add(b3);
+		add(b4);
 
-	setSize(800,600);
+		b2.addActionListener(new CloseListener());
 
-	setLayout(new GridLayout(5,2));
+		setSize(800,600);
 
-	setTitle("Lottery Ticket Generator");
+		setLayout(new GridLayout(5,2));
+
+		setTitle("Lottery Ticket Generator");
+		
+		MegaMillionsButton.addActionListener(this);
+		Fantasy5Button.addActionListener(this);
+		PowerballButton.addActionListener(this);
 	}
 
-
 	
+	//this is what i seem to be having a problem with but not sure
+	//might need someone to look it over
+	//but i think its in the right direction. maybe?
+	public static void clickButton(ActionEvent event) {
+		if(event.getSource() == MegaMillionsButton) {
+			
+			JOptionPane.showMessageDialog(frame, "Great choice! Please enter numbers on next screen.");
+		}else if(event.getSource() == Fantasy5Button) {
+			
+			JOptionPane.showMessageDialog(frame, "Great choice! Please enter numbers on next screen.");
+		} else if(event.getSource() == PowerballButton) {
+			
+			JOptionPane.showMessageDialog(frame, "Great choice! Please enter numbers on next screen.");
+		}else {
+			JOptionPane.showMessageDialog(frame, "You have not made a decision.");
+			}
+		}
+
+
 	public static void main(String[] args) {
-		
+
 		gameChoicePanel = new JPanel(new GridLayout(4,4,4,4));
-        gameChoiceJFrame = new JFrame();
-        gameChoiceJFrame.setPreferredSize(new Dimension(500, 600));
-        gameChoiceJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameChoiceJFrame.getContentPane().add(gameChoicePanel);
-        gameChoiceJFrame.pack();
-        
-        question = new JLabel("Please choose a lottery: ");
-        question.setHorizontalAlignment(JLabel.CENTER);
-        gameChoicePanel.add(question);
+		gameChoiceJFrame = new JFrame();
+		gameChoiceJFrame.setPreferredSize(new Dimension(500, 600));
+		gameChoiceJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameChoiceJFrame.getContentPane().add(gameChoicePanel);
+		gameChoiceJFrame.pack();
+
+		question = new JLabel("Please choose a lottery: ");
+		question.setHorizontalAlignment(JLabel.CENTER);
+		gameChoicePanel.add(question);
 		MegaMillionsButton = new JButton(megaMillionsIcon);
-	    MegaMillionsButton.setSize(100 , 30);
+		MegaMillionsButton.setSize(100 , 30);
 		gameChoicePanel.add(MegaMillionsButton);
 		Fantasy5Button = new JButton(fantasy5Icon);
 		Fantasy5Button.setSize(100 , 30);
@@ -87,32 +110,26 @@ public class GameChoice extends JFrame implements ActionListener  {
 		gameChoiceJFrame.add(gameChoicePanel);
 		gameChoiceJFrame.setVisible(true);
 		
-		//trying to set where the button is selected then it brings up the other window.
-		//having a little bit of trouble.
-		if(MegaMillionsButton.isSelected() || Fantasy5Button.isSelected() || PowerballButton.isSelected()) {
-			JOptionPane.showMessageDialog(frame, "Great choice! Please enter numbers on next screen.");
-		}else if(!MegaMillionsButton.isSelected() || !Fantasy5Button.isSelected() || !PowerballButton.isSelected()) {
+		//got it halfway working
+		ActionEvent event1 = null;
+		clickButton(event1);
 		
-		} else {
-			JOptionPane.showMessageDialog(frame, "You have not made a decision.");
-		}
 		GameChoice a = new GameChoice();
 		a.setVisible(true);
 		a.setLocation(200, 200);
-		
-		
+
+
 	}
-	 public void actionPerformed(ActionEvent ae)
+	public void actionPerformed(ActionEvent ae)
 	{
-	float a, b;
+		float a, b;
 	}
-	 
 	//This is to exit the program.
 	class CloseListener implements ActionListener
 	{
-	public void actionPerformed(ActionEvent e) 
-	{
-	System.exit(0);
-	} 
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.exit(0);
+		} 
 	}
 }
