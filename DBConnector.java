6ee28946952ec576e4b0e7033596ac73a0dd7050
fsanchez4 <33576135+fsanchez4@users.noreply.github.com>
2018-07-16 14:55:22 -0400

    Update DBConnector.java

--- DBConnector.java
+++ DBConnector.java
@@ -4,21 +4,31 @@ import java.sql.*;
 
 public class DBConnector {
 
-    private static String url = "jdbc:mysql://localhost:3306/lottery_database?allowPublicKeyRetrieval=true&useSSL=false&requireSSL=false";
-    private static String user = "root";
-    private static String password = "Fam0uscl0ud#1";
+    private static String url = "jdbc:mysql://sql9.freemysqlhosting.net:3306/sql9247157?allowPublicKeyRetrieval=true&useSSL=false&requireSSL=false";
+    private static String user = "sql9247157";
+    private static String password = "ZY9rN5phwb";
+
+    /*
+    Mega Millions table:  mm_table
+    Powerball table:  powerball_table
+    MM user quick picks table:  mm_quickpicks
+    PB user quick picks table:  powerball_quickpicks
+    Primary key (column):  id
+    Columns:  id, date, num_1, num_2, num_3, num_4, num_5, money_ball, multiplier
+    */
+
 
     static void dbConnect() {
         try {
 
             // Get a connection to database
             Connection myConn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection complete.");
 
-        } catch (Exception exc) {
+        } catch (SQLException exc) {
             exc.printStackTrace();
 
         }
-        System.out.println("Connection complete.");
     }
 
     public static void main(String[] args) {
